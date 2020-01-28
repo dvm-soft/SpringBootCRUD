a468f3e3ab123f7683265f483e08b64d55fd04fd
dvm-soft <dmikhailov@mail.ru>
2020-01-28 07:14:16 +0300

    Работает

--- src/main/java/com/dvm/SpringBootCrudApplication.java
+++ src/main/java/com/dvm/SpringBootCrudApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrusApplication {
+public class SpringBootCrudApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootCrusApplication.class, args);
+        SpringApplication.run(SpringBootCrudApplication.class, args);
     }
 }
